0d66fece7ef58648b20869435df9671f0554a7bd
bobbythree <bobbythr33@gmail.com>
2026-05-08 20:39:09 -0500

    add preposition file, fix parser and command struct

--- parser/parser.go
+++ parser/parser.go
@@ -3,20 +3,28 @@ package parser
 import "strings"
 
 type Command struct {
-	Verb string
-	Noun string
+	Verb           string
+	DirectObject   string
+	Prepositions   string
+	IndirectObject string
 }
 
 func Parse(input string) Command {
 	words := strings.Fields(strings.ToLower(input))
 	cmd := Command{}
+	verb, ok := VerbAliases[words[0]]
+	if ok {
+		cmd.Verb = verb
+	} else {
+		cmd.Verb = words[0]
+	}
 
 	if len(words) > 0 {
 		cmd.Verb = words[0]
 	}
 
 	if len(words) > 1 {
-		cmd.Noun = words[1]
+		cmd.DirectObject = words[1]
 	}
 
 	return cmd
