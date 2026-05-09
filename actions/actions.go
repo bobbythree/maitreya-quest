8d77c7eb273c836a6836b55951a365bceea410ff
bobbythree <bobbythr33@gmail.com>
2026-05-08 20:31:39 -0500

    move aliases from actions map to vocab

--- actions/actions.go
+++ actions/actions.go
@@ -11,11 +11,7 @@ type ActionFunc func(*game.GameState, parser.Command)
 var ActionMap = map[string]ActionFunc{
 	"look":      Look,
 	"get":       Get,
-	"take":      Get,
 	"open":      Open,
 	"close":     Close,
-	"shut":      Close,
 	"inventory": Inventory,
-	"inv":       Inventory,
-	"i":         Inventory,
 }
--- parser/vocabulary.go
+++ parser/vocabulary.go
@@ -1,1 +1,20 @@
 package parser
+
+var VerbAliases = map[string]string{
+	"look":    "look",
+	"examine": "look",
+	"inspect": "look",
+
+	"get":  "get",
+	"take": "get",
+	"grab": "get",
+
+	"open": "open",
+
+	"close": "close",
+	"shut":  "close",
+
+	"inventory": "inventory",
+	"inv":       "inventory",
+	"i":         "inventory",
+}
