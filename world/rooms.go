e1a17016cddeae7d34be2fd6b6b69b3002807da2
bobbythree <bobbythr33@gmail.com>
2026-06-17 21:01:38 -0500

    add man object to street

--- world/rooms.go
+++ world/rooms.go
@@ -37,6 +37,9 @@ var Rooms = map[string]Room{
 		ID:          "street",
 		Name:        "street",
 		Description: "The street outside your apartment is quiet. You see a man standing on the corner.",
+		Objects: []string{
+			"man",
+		},
 		Exits: map[string]string{
 			"west": "hallway",
 		},
